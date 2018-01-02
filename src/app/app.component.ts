2ec0d501e3d6361fea791037841b253ee133dac1
JHades <jhades.dev@gmail.com>
2018-01-02 16:40:00 +0100

    Angular PWA Course

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {Observable} from "rxjs/Observable";
 import {SwPush, SwUpdate} from "@angular/service-worker";
-import {MatSnackBar} from "@angular/material/snack-bar";
 
 @Component({
   selector: 'app-root',
