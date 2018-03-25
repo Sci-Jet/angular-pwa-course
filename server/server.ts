d7365015cb9430206f7b1d7270963de6a78f1b88
Xu Yixin <xuyixin0723@gmail.com>
2018-03-25 15:09:12 +0800

    修改部分格式问题

--- server/server.ts
+++ server/server.ts
@@ -1,7 +1,7 @@
 
 import * as express from 'express';
-import {Application} from "express";
-import {readAllLessons} from "./read-all-lessons.route";
+import { Application } from 'express';
+import { readAllLessons } from './read-all-lessons.route';
 const bodyParser = require('body-parser');
 
 
@@ -19,7 +19,7 @@ app.route('/api/lessons')
 
 // launch an HTTP Server
 const httpServer = app.listen(9000, () => {
-    console.log("HTTP Server running at http://localhost:" + httpServer.address().port);
+    console.log('HTTP Server running at http://localhost:' + httpServer.address().port);
 });
 
 
